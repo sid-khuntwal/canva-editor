10f2f02e240e1c9bb4ce01f6e1c4e55c4f2969ca
Siddharth Khuntwal <achillesid34@gmail.com>
2024-01-24 13:37:56 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
+import "./index.css";
 import { templateData } from "./templateData";
 
 const App = () => {
   const canvasRef = useRef(null);
+  const hiddenFileInput = useRef(null);
   const [buttonText, setButtonText] = useState("Contact Us");
   const [captionText, setCaptionText] = useState(
     "1 & 2 BHK Luxury Apartments at just Rs.34.97 Lakhs"
   );
   const [imgUrl, setImgUrl] = useState(
-    "https://cf.bstatic.com/xdata/images/hotel/max1024x768/383834719.jpg?k=a8ed632aeaf2eb621e6753e941d4fb2f858005614b603cdef5bfe644ce1a1906&o=&hp=1"
+    "https://static.tnn.in/photo/msid-107047749/107047749.jpg"
   );
+
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -33,6 +39,8 @@ const App = () => {
     drawButton(context);
     drawCaption(context);
   }, []);
+
+
 
   const drawImage = () => {
     const canvas = canvasRef.current;
@@ -72,7 +80,6 @@ const App = () => {
     const buttonX = canvasRef.current.width - 200;
     const buttonY = canvasRef.current.height - 150;
 
-
     context.fillStyle = "#3498db";
     context.fillRect(buttonX, buttonY, buttonWidth, buttonHeight);
 
@@ -100,29 +107,58 @@ const App = () => {
   // Caption
 
   const drawCaption = (context) => {
-    const buttonWidth = 200;
-    const buttonHeight = 100;
-    const buttonX = 80;
-    const buttonY = canvasRef.current.height - 150;
 
     context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 
-    context.fillStyle = "pink";
-    context.fillRect(buttonX, buttonY, buttonWidth, buttonHeight);
+
 
     context.fillStyle = "#000";
-    context.font = "16px Arial";
-    context.textAlign = "right";
-    context.textBaseline = "bottom";
-    context.fillText(
+    context.font = "20px Arial";
+    context.textAlign = "left";
+    // context.textBaseline = "bottom";
+
+    const lines = textLines(
       captionText,
-      buttonX + buttonWidth / 2,
-      buttonY + buttonHeight / 2
+      templateData.caption.max_characters_per_line
     );
+
+    let y = canvasRef.current.height - 135;
+    const x = 30;
+
+
+    lines.forEach((line) => {
+      context.fillText(line, x, y);
+      y += 22;
+    });
+
+
 
     drawImage();
     drawButton(context);
   };
+
+  const textLines = (text, maxLines) => {
+    const words = text.split(" ");
+    const lines = [];
+    let currentLine = "";
+
+    words.forEach((word) => {
+      const Line = currentLine ? `${currentLine} ${word}` : word;
+
+      if (Line.length <= maxLines) {
+        currentLine = Line;
+      } else {
+        lines.push(currentLine);
+        currentLine = word;
+      }
+    });
+
+    if (currentLine) {
+      lines.push(currentLine);
+    }
+
+    return lines;
+  }
 
   const handleCaptionChange = (event) => {
     setCaptionText(event.target.value);
@@ -141,41 +177,63 @@ const App = () => {
           <canvas ref={canvasRef} />
         </div>
       </div>
-      <div className="panel-parent">
-        <div className="panel-container">
-          <div className="panel-heading">
-            <div>
-              <h3>AD Customization</h3>
-            </div>
 
-            <div>XYZ</div>
-          </div>
-          <div className="panel-content">
-            <div>
+      <div className="panel-parent">
+        <div className=" text-center p-2 mb-10">
+          <h1 className="p-1 mt-5 font-semibold text-2xl">Ad customisation</h1>
+          <p className="text-gray-400 text-sm">
+            Customise your add and get the templates accordingly
+          </p>
+        </div>
+
+        <div className="border-2 p-3 rounded-xl">
+          <div className="flex items-center">
+            <p className="text-sm">Change AD image</p>
+            <p
+              className="ml-1 underline text-blue-700 cursor-pointer"
+              onClick={handleClick}
+            >
+              Select
               <input
+                ref={hiddenFileInput}
+                className="hidden"
                 type="file"
                 accept="image/*"
                 onChange={(event) => handleImgChange(event)}
               />
-            </div>
+            </p>
+          </div>
+        </div>
 
-            <div>
-              <label>CTA</label>
-              <input
-                type="text"
-                value={buttonText}
-                onChange={(event) => handleBtnInput(event)}
-              />
-            </div>
+        <div className="flex py-5 items-center my-5">
+          <div className="flex-grow border-t border-gray-400"></div>
+          <span className="flex-shrink mx-4 text-gray-400 text-sm">
+            Edit Contents
+          </span>
+          <div className="flex-grow border-t border-gray-400"></div>
+        </div>
 
-            <div>
-              <label>Caption Text:</label>
+        <div className=" flex flex-col gap-2">
+          <div className="border-2 py-2 px-4 rounded-xl">
+            <p className="text-gray-500 text-sm w-full">Caption</p>
+            <div className="flex">
               <input
+                className="text-md w-full focus:border-transparent focus:outline-none"
                 type="text"
                 value={captionText}
                 onChange={(event) => handleCaptionChange(event)}
               />
             </div>
+          </div>
+
+          <div className="border-2 px-4 py-2 rounded-xl">
+            <p className="text-gray-500 text-sm w-full">CTA</p>
+            <input
+              className="text-md w-full focus:border-transparent focus:outline-none"
+              type="text"
+              value={buttonText}
+              onChange={(event) => handleBtnInput(event)}
+            />
           </div>
         </div>
       </div>
